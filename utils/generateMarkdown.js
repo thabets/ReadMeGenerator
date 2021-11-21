1eead91058239208d6c7890c020a0be8c403ec9a
Sam Thabet <sallam.thabet@gmail.com>
2021-11-21 00:08:34 -0500

    cleaned up code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -42,27 +42,6 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function renderLicenseSection(license) {
-//   if (license == "None") {
-//     return "";
-//   } else if (license == "MIT") {
-//     return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-//   } else if (license == "IBM") {
-//     return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
-//   } else if (license == "ISC") {
-//     return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-//   } else if (license == "Mozilla") {
-//     return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-//   } else if (license == "Apache 2.0") {
-//     return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-//   } else if (license == "Boost") {
-//     return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-//   } else if (license == "BSD") {
-//     return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-//   }
-// }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
